7ec1c1ecb5213ee2d7a4cfc1e651d487137d614e
feiyu_24 <Mr.zhu345@gmail.com>
2025-02-25 18:09:10 +0800

    02/25/gazebo

--- src/urdf_agv_description/launch/gazebo.launch.py
+++ src/urdf_agv_description/launch/gazebo.launch.py
@@ -1,4 +1,6 @@
 import os
+import launch
+import launch_ros
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
@@ -9,11 +11,30 @@ def generate_launch_description():
     robot_name_in_model = 'fishbot'
     package_name = 'urdf_agv_description'
     urdf_name = "agv_base_gazebo.urdf"
-
-    ld = LaunchDescription()
     pkg_share = FindPackageShare(package=package_name).find(package_name) 
     urdf_model_path = os.path.join(pkg_share, f'urdf/{urdf_name}')
 
+    # 为 Launch 声明参数
+    action_declare_arg_mode_path = launch.actions.DeclareLaunchArgument(
+        name='model', default_value=str(urdf_model_path),
+        description='URDF 的绝对路径')
+    # 获取文件内容生成新的参数
+    robot_description = launch_ros.parameter_descriptions.ParameterValue(
+        launch.substitutions.Command(
+            ['cat ', launch.substitutions.LaunchConfiguration('model')]),
+        value_type=str)
+
+    # 状态发布节点
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': robot_description}]
+    )
+    # 关节状态发布节点
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+    )
     # Start Gazebo server
     start_gazebo_cmd =  ExecuteProcess(
         cmd=['gazebo', '--verbose','-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],
@@ -25,6 +46,10 @@ def generate_launch_description():
         executable='spawn_entity.py',
         arguments=['-entity', robot_name_in_model,  '-file', urdf_model_path ], output='screen')
 
+    ld = LaunchDescription()
+    ld.add_action(action_declare_arg_mode_path)
+    ld.add_action(joint_state_publisher_node)
+    ld.add_action(robot_state_publisher_node)
     ld.add_action(start_gazebo_cmd)
     ld.add_action(spawn_entity_cmd)
 

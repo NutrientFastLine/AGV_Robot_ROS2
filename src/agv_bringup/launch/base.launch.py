73d4d5971db2d85075adf9b6b0d8e26f73424581
feiyu_nx <Mr.zhu345@gmail.com>
2025-03-07 10:55:20 +0800

    0307

--- src/agv_bringup/launch/base.launch.py
+++ src/agv_bringup/launch/base.launch.py
@@ -3,11 +3,7 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.substitutions import FindPackageShare
-from launch.actions import DeclareLaunchArgument
 from ament_index_python.packages import get_package_share_directory
-
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
 
--- src/agv_bringup/launch/sllidar.launch.py
+++ src/agv_bringup/launch/sllidar.launch.py
@@ -2,11 +2,30 @@
 
 import os
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-#==========启动robot_desscription URDF文件========================================================
+    #==========启动robot_desscription URDF文件========================================================
+    
+    # description_pkg_share = FindPackageShare(package='urdf_agv_description').find('urdf_agv_description') 
+    # description_model_path = os.path.join(description_pkg_share, f'urdf/{"agv_base.urdf"}')
+
+    # urdf_model = LaunchConfiguration('model',default=description_model_path)
+
+    # robot_description = {
+    #     'robot_description': Command(['cat ', urdf_model])
+    # }
+    # robot_state_publisher_node = Node(
+    #     package='robot_state_publisher',
+    #     executable='robot_state_publisher',
+    #     parameters=[robot_description]
+    # )
+    # joint_state_publisher_node = Node(
+    #     package='joint_state_publisher',
+    #     executable='joint_state_publisher',
+    # )
 
     channel_type =  LaunchConfiguration('channel_type', default='serial')
     serial_port = LaunchConfiguration('serial_port', default='/dev/sllidar_base')
@@ -33,6 +52,10 @@ def generate_launch_description():
         output='screen',
         )
     ld = LaunchDescription()
+
+    # ld.add_action(joint_state_publisher_node)
+    # ld.add_action(robot_state_publisher_node)
+    
     ld.add_action(sllidar_ros2_node)
 
     return ld
